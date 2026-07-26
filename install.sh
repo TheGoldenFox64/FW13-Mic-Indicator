77b64b32b69bd01071b36e31b22c1d0183ac3b9a
TheGoldenFox64 <131229529+TheGoldenFox64@users.noreply.github.com>
2026-07-26 02:12:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo mv mic-mute-monitor.sh /usr/local/sbin/
 sudo chmod +x /usr/local/sbin mic-mute-monitor.sh
+sudo mv mic-mute-monitor.sh /usr/local/sbin/
 sudo mv micmonitor.service /etc/systemd/system/
